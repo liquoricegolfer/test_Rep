a6dc5b7758de1041ce2a18433732368e996363f9
PhilippHTLAnich <phibock@tsn.at>
2021-06-15 09:11:04 +0200

    Initial Commit "new Tetris"

--- Tetris_new.py
+++ Tetris_new.py
@@ -3,41 +3,39 @@ pygame.init()
 
 WHITE = (255,255,255)
 TURQUOISE = (64,224,208)
-PINK (0,0,254)
+PINK = (0,0,254)
 BLACK = (0,0,0)
 
 display = pygame.display.set_mode ((1280,720))
 pygame.display.set_caption("Tetris")
 
-spielaktiv = True
+activegame= True
 
 clock = pygame.time.Clock()
 
-while spielaktiv:
+while activegame:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            spielaktiv = False
-            print("Spieler hat Quit-Button angeklickt")
-        elif event.type == pygame.KEYDOWN:
-            print("Spieler hat Taste gedrückt")
-
-            elif event.key == pygame.K_w:
-                print("Spieler hat Taste w gedrückt")
-            elif event.key == pygame.K_a:
-                print("Spieler hat Taste a gedrückt")
-            elif event.key == pygame.K_s:
-                print("Spieler hat Taste s gedrückt")
-            elif event.key == pygame.K_d:
-                print("Spieler hat Taste d gedrückt")
+            activegame = False
+            print("Quit")
+            if event.key == pygame.K_RIGHT:
+                print("RIGHT")
+            elif event.key == pygame.K_LEFT:
+                print("LEFT")
+            elif event.key == pygame.K_UP:
+                print("UP")
+            elif event.key == pygame.K_DOWN:
+                print("DOWN")
+            elif event.key == pygame.K_SPACE:
+                print("SPACE")
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print("Spieler hat Maus angeklickt")
+            print("MOUSE")
 
-    screen.fill(WHITE)
+    display.fill(WHITE)
 
     pygame.display.flip()
 
     clock.tick(60)
 
 pygame.quit()
-
